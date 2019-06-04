538f521370858aaee61f4b8de967926ffe9dc0de
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-06-04 15:06:02 +0200

    add notepost method

--- routes/note.js
+++ routes/note.js
@@ -1,10 +1,26 @@
 const express = require('express');
 
+const NotePost = require('../models/NotePost');
+
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.post('/', (req, res, next) => {
+  const { title, content } = req.body;
+  const note = new NotePost({
+    title,
+    content,
+  });
+  const saveNote = note.save()
+      .then((response) => {
+      res.status(200)
+      res.json(response)
+    })
+    .catch(next)
 });
 
 module.exports = router;
