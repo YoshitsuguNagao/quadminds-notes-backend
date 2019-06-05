211e5d348f2ace1573f16fe3d3b8e44b937b099b
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-06-05 20:01:37 +0200

    change file name

--- routes/note.js
+++ routes/note.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Note = require('../models/Note');
+const Note = require('../models/note');
 
 
 router.get('/', (req, res, next) => {
